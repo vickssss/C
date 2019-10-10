4402c8b100a64f5c3ac5374932e4ce10afc3e84a
Shekhar <rkshekhar015@gmail.com>
2019-10-10 11:53:47 +0530

    Decimal vs Octal vs Hexadecimal

---
+++ Decimal_to_Hexadecimal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;    // Any Number
+    printf("Enter a number :");
+    scanf("%d",&n);
+    printf("\nValue of decimal number :%d",n);
+    printf("\nValue of Octal number :%o",n);
+    printf("\nValue of Hexadecimal :%x or %X",n,n);
+}
+// Enter a number :2567
+
+// Value of decimal number :2567
+// Value of Octal number :5007
+// Value of Hexadecimal :a07 or A07
