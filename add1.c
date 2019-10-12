d4ce53bbde211d7acbe7bb4c50308a33b1b0aafe
Shekhar <rkshekhar015@gmail.com>
2019-10-12 20:00:32 +0530

    Add

---
+++ add1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    printf("Enter number");
+    scanf("%d",&a);
+    printf("Enter number");
+    scanf("%d",&b);
+    printf("Enter number");
+    scanf("%d",&c);
+    printf("Enter number");
+    scanf("%d",&d);
+    printf("Enter number");
+    scanf("%d",&e);
+    int z=a+b+c+d+e;
+    printf("%d",z);
+}
