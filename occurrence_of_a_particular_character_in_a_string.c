896fe17fb08c0ce479c21920b09fb45409cd850e
Shekhar <rkshekhar015@gmail.com>
2019-10-14 00:24:05 +0530

    Enter a string :Shekhar kumar

---
+++ occurrence_of_a_particular_character_in_a_string.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    char ch,a[100];
+    int count=0;
+    printf("Enter a string :");
+    gets(a);
+    printf("\nEnter a character :");
+    scanf("%c",&ch);
+    for(int i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==ch)
+        count++;
+    }
+    printf("\n%c found %d times in %s",ch,count,a);
+}
+// Enter a string :Shekhar kumar
+
+// Enter a character :k
+
+// k found 2 times in Shekhar kumar
